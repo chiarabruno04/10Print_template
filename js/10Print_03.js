8ad822abc2188f64510c73399ebf74f4a54382d8
chiarabruno00 <brunochiara2004@gmail.com>
2024-05-21 14:50:26 +0200

    initial

--- js/10Print_03.js
+++ js/10Print_03.js
@@ -1,28 +1,35 @@
-let x = 0;
-let y = 0;
-let size = 30;
+let x=0;
+let y=0;
+let size=30;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(245);
-}
+ function setup() {
+   createCanvas(windowWidth, windowHeight);
+   background(222);
+  
+   
+ }
 
 function draw() {
-  strokeWeight(0);
-  stroke(400, 0, 0);
-  fill(200, 0, 0, 50);
+  strokeWeight(2);
+  stroke (0,0,10)
+fill (0,0,20,20)
 
-  if (random(1) < 0.5) {
-    ellipse(x - 50, y, 40);
-    circle(x - 20, y, 45);
-    stroke(200, 100, 0);
-  } else {
-    ellipse(x, y);
-  }
-
-  x += size;
-  if (x > width) {
-    x = 0;
+  
+  
+  if(random(1) < 2.5){ 
+    ellipse(x-50,y,40)
+     circle(x-20, y, 40);
+    stroke(0,100,0);
+    } else { 
+      ellipse(x,y);
+      
+    
+        }
+  
+  x +=size;
+  if(x > width){ 
+    x= 0;
     y = y + size;
   }
+  
 }
